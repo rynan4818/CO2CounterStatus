96141bbf6fe85fe00a68b01f8d08a0dda1daba2a
rynan4818 <14249877+rynan4818@users.noreply.github.com>
2023-02-18 17:09:34 +0900

    CO2Counter完成

---
+++ CO2Counter/CO2Counter.cs
@@ -0,0 +1,49 @@
+﻿using CO2Core.Interfaces;
+using CO2Counter.Configuration;
+using CountersPlus.Counters.Custom;
+using System.Globalization;
+using TMPro;
+using UnityEngine;
+
+namespace CO2Counter
+{
+    internal class CO2Counter : BasicCustomCounter
+    {
+        private readonly ICO2CoreManager _manager;
+        private float x = PluginConfig.Instance.OffsetX;
+        private float y = PluginConfig.Instance.OffsetY;
+        private float z = PluginConfig.Instance.OffsetZ;
+        private TMP_Text _counterCO2;
+        public CO2Counter(ICO2CoreManager manager)
+        {
+            this._manager = manager;
+        }
+        public override void CounterInit()
+        {
+            this._manager.OnCO2Changed += this.OnCO2Changed;
+            if (PluginConfig.Instance.EnableLabel)
+            {
+                var label = CanvasUtility.CreateTextFromSettings(Settings, new Vector3(x, y, z));
+                label.text = PluginConfig.Instance.LabelText;
+                label.fontSize = PluginConfig.Instance.LabelFontSize;
+            }
+            _counterCO2 = CanvasUtility.CreateTextFromSettings(Settings, new Vector3(x, y - 0.2f, z));
+            _counterCO2.lineSpacing = -26;
+            _counterCO2.fontSize = PluginConfig.Instance.FigureFontSize;
+            _counterCO2.alignment = TextAlignmentOptions.Top;
+            OnCO2Changed(0, 0, 0);
+        }
+        public override void CounterDestroy()
+        {
+            this._manager.OnCO2Changed -= this.OnCO2Changed;
+        }
+        private void OnCO2Changed(int co2, double hum, double tmp)
+        {
+            _counterCO2.text = $"{co2}<size=50%>ppm</size>  {StringFormat(tmp)}<size=50%>℃</size>  {StringFormat(hum)}<size=50%>%</size>";
+        }
+        private string StringFormat(double distance)
+        {
+            return $"{distance.ToString($"F{PluginConfig.Instance.DecimalPrecision}", CultureInfo.InvariantCulture)}";
+        }
+    }
+}
--- CO2Counter/Plugin.cs
+++ CO2Counter/Plugin.cs
@@ -1,4 +1,6 @@
 ﻿using IPA;
+using IPA.Config;
+using IPA.Config.Stores;
 using IPALogger = IPA.Logging.Logger;
 
 namespace CO2Counter
@@ -7,29 +9,15 @@ namespace CO2Counter
     public class Plugin
     {
         internal static Plugin Instance { get; private set; }
-        /// <summary>
-        /// Use to send log messages through BSIPA.
-        /// </summary>
         internal static IPALogger Log { get; private set; }
-
         [Init]
-        public Plugin(IPALogger logger)
+        public Plugin(IPALogger logger, Config conf)
         {
             Instance = this;
             Log = logger;
+            Log.Info("CO2Counter initialized.");
+            Configuration.PluginConfig.Instance = conf.Generated<Configuration.PluginConfig>();
+            Log.Debug("Config loaded");
         }
-
-        [OnStart]
-        public void OnApplicationStart()
-        {
-            Plugin.Log.Info("OnApplicationStart");
-        }
-
-        [OnExit]
-        public void OnApplicationQuit()
-        {
-
-        }
-
     }
 }
