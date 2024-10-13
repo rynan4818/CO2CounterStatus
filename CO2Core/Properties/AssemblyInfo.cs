1d8fe229cd676aceded0aa3e74282cf41aa8972b
rynan4818 <14249877+rynan4818@users.noreply.github.com>
2024-10-13 11:27:16 +0900

    VersionUP

--- CO2Core/Properties/AssemblyInfo.cs
+++ CO2Core/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("CO2Core")]
-[assembly: AssemblyCopyright("Copyright ©  2023 Rynan4818")]
+[assembly: AssemblyCopyright("Copyright ©  2024 Rynan4818")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.1.4")]
-[assembly: AssemblyFileVersion("1.1.4")]
+[assembly: AssemblyVersion("1.1.5")]
+[assembly: AssemblyFileVersion("1.1.5")]
